aa3728737ffacfc95020000ff990da1c28b9d4ba
Ester Castellano Rios <escastel@c2r5s2.42malaga.com>
2024-06-07 18:33:14 +0200

    ex02

---
+++ Module_01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: escastel <escastel@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 16:11:12 by escastel          #+#    #+#             */
+/*   Updated: 2024/06/07 18:31:43 by escastel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void){
+	std::string		stringSRC = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &stringSRC;
+	std::string&	stringREF = stringSRC;
+
+	std::cout << "\nMemory address\nstringSRC: " << &stringSRC << std::endl;
+	std::cout << "stringPTR: " << stringPTR << std::endl;
+	std::cout << "stringREF: " << &stringREF << "\n" << std::endl;
+
+	std::cout << "Value\nstringSRC: " << stringSRC << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << "\n" << std::endl;
+}
