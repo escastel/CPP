2b150f9e4a3944eb738ec2d3c1ab6a8144eb3c71
Ester Castellano Rios <escastel@c3r8s5.42malaga.com>
2024-06-18 17:00:13 +0200

    .

--- Module_03/ex01/main.cpp
+++ Module_03/ex01/main.cpp
@@ -6,19 +6,17 @@
 /*   By: escastel <escastel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 18:02:41 by escastel          #+#    #+#             */
-/*   Updated: 2024/06/14 12:40:31 by escastel         ###   ########.fr       */
+/*   Updated: 2024/06/18 16:59:20 by escastel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 int	main(){
-	ClapTrap coco("Coco");
-	coco.takeDamage(5);
-	coco.attack("Momo");
-	coco.takeDamage(2);
-	coco.beRepaired(2);
-	coco.takeDamage(5);
-	coco.attack("Momo");
+	ScavTrap momo("Momo");
+	momo.attack("Coco");
+	momo.takeDamage(6);
+	momo.beRepaired(4);
+	momo.takeDamage(20);
 	return 0;
 }
